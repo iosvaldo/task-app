6d5e76548a4dac03333e4d08271a40925ca730c6
osvaldo <iosvaldo.app@gmail.com>
2023-04-17 21:26:37 -0400

    aded CRUD functionality 🦚

--- src/router/index.js
+++ src/router/index.js
@@ -5,19 +5,20 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: TodoView
+      path: "/",
+      name: "home",
+      component: TodoView,
     },
     {
-      path: '/about',
-      name: 'about',
+      path: "/about",
+      name: "about",
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
-})
+      component: () => import("../views/AboutView.vue"),
+    },
+   
+  ],
+});
 
 export default router
